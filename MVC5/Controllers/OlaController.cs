4137fec11a4cb426a26fa9cd348785d44424541c
jmfrolim <joao.mfreitas@live.com>
2015-06-02 20:59:03 -0300

    Criacao da model livros e do controller Livroes com CRUD

---
+++ MVC5/Controllers/OlaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class OlaController : Controller
+    {
+        // GET: Ola
+        public string Index()
+        {
+            return "Olá MVC5";
+        }
+
+        public string Teste()
+        {
+            return "Teste feito <b>texto negrito como da video aula!</b>";
+        }
+
+        public JsonResult GetJson()
+        {
+            var produto = new
+            {
+                ID = 12,
+                Nome= "Bike",
+                Qtde= 10
+            };
+            return Json(produto, JsonRequestBehavior.AllowGet);
+        }
+
+        public string GetDados(string nome, int idade =20)
+        {
+            return HttpUtility.HtmlEncode( string.Format("BemVido {0} com idade{1}", nome, idade));
+        }
+    }
+}
---
+++ MVC5/Models/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC5.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage="Digite o titulo do texto!")]
+        [StringLength(100, ErrorMessage="O tamannho maximo de caracteres é {0}")]
+        public string Titulo { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+
+        public bool Disponibilidade { get; set; }
+
+
+    }
+}
